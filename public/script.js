26fe7797f9ecb0216b353cea056f903d227a2d43
mukund <mukundt513@gmail.com>
2025-11-11 02:00:18 +0530

    add chat-message

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-// const socket = io("https://google-meet-8dov.onrender.com/");
-const socket = io("http://localhost:3000");
+ const socket = io("https://google-meet-8dov.onrender.com/");
+
 const roomInput = document.getElementById("roomInput");
 const joinBtn = document.getElementById("joinBtn");
 const videos = document.getElementById("videos");
